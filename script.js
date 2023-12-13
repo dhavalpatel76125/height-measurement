249a73ae4d2361deebaa36a69368cd35bc3d7cfa
ankitebiztrait <141809293+ankitebiztrait@users.noreply.github.com>
2023-12-13 16:16:40 +0530

    d

--- script.js
+++ script.js
@@ -1,52 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const videoElement = document.getElementById("myVideo");
-    const slider = document.getElementById("mySlider");
-    const label = document.getElementById("myLabel");
-    const heightInfo = document.getElementById("heightInfo");
-    let calibratedBeta = 0;
-
-    function initCamera() {
-        navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
-            .then(function(stream) {
-                videoElement.srcObject = stream;
-                videoElement.play();
-            })
-            .catch(function (err) {
-                alert("Error accessing camera: " + err.message);
-            });
+function onOrientationChange(event) {
+    let angle = event.beta - 90;
+    if (angle < 0) {
+        angle = 0;
     }
 
-    function calculateHeight(angle, distance) {
-        return Math.tan(angle * Math.PI / 180) * distance;
-    }
+    const distToTree = parseFloat(document.getElementById("mySlider").value);
+    document.getElementById("myLabel").innerHTML =
+        "Distance to tree: " + distToTree + " meters";
 
-    function metersToCentimeters(meters) {
-        return meters * 100;
-    }
+    // Calculate height in meters
+    const heightInMeters = Math.tan(angle * (Math.PI / 180)) * distToTree;
 
-    function metersToFeet(meters) {
-        return meters * 3.28084;
-    }
+    // Convert height to centimeters and feet
+    const heightInCentimeters = heightInMeters * 100;
+    const heightInFeet = heightInMeters * 3.28084;
 
-    function updateDisplay(event) {
-        const angle = Math.max(event.beta - calibratedBeta - 90, 0);
-        const distance = slider.value;
-        const heightInMeters = calculateHeight(angle, distance);
-        const heightInCentimeters = metersToCentimeters(heightInMeters);
-        const heightInFeet = metersToFeet(heightInMeters);
-
-        label.textContent = `Distance to object: ${distance} meters`;
-        heightInfo.textContent = `${heightInMeters.toFixed(2)} m / ${heightInCentimeters.toFixed(1)} cm / ${heightInFeet.toFixed(2)} ft (${angle.toFixed(1)}°)`;
-    }
-
-    function calibrateOrientation(event) {
-        calibratedBeta = event.beta;
-        alert("Calibration complete. Please point your device towards the top of the object.");
-    }
-
-    window.addEventListener("deviceorientation", updateDisplay);
-    slider.addEventListener("input", updateDisplay);
-    videoElement.addEventListener("click", calibrateOrientation);
-
-    initCamera();
-});
+    // Display height in meters, centimeters, and feet
+    document.getElementById("heightInfo").innerHTML =
+        heightInMeters.toFixed(1) + " m (" + angle.toFixed(1) + "&deg;)<br>" +
+        heightInCentimeters.toFixed(1) + " cm<br>" +
+        heightInFeet.toFixed(2) + " ft";
+}
